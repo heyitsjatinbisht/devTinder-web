212cccdced21308f0e71b5fe20ec942e441d773a
Jsbist21 <bishtjatin477@gmail.com>
2024-11-10 11:50:09 +0530

    feat: accept or reject requests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,30 +1,47 @@
 import axios from "axios";
-import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
+import { useEffect, useState } from "react";
 
 const Requests = () => {
   const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
 
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {}
+  };
+
   const fetchRequests = async () => {
     try {
-      const res = axios.get(BASE_URL + "/user/requests/recieved", {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
         withCredentials: true,
       });
+
       dispatch(addRequests(res.data.data));
-    } catch (error) {}
+    } catch (err) {}
   };
 
   useEffect(() => {
     fetchRequests();
   }, []);
+
   if (!requests) return;
-  if (requests.length === 0) return <h1>No Rerquests Found</h1>;
+
+  if (requests.length === 0)
+    return <h1 className="flex justify-center my-10"> No Requests Found</h1>;
+
   return (
     <div className="text-center my-10">
-      <h1 className="text-bold text-white text-4xl">Connection Requests</h1>
+      <h1 className="text-bold text-white text-3xl">Connection Requests</h1>
+
       {requests.map((request) => {
         const { _id, firstName, lastName, photoUrl, age, gender, about } =
           request.fromUserId;
@@ -32,19 +49,35 @@ const Requests = () => {
         return (
           <div
             key={_id}
-            className="flex justify-between items-center m-4 p-4 border rounded-lg bg-base-300 w-2/3 mx-auto"
+            className=" flex justify-between items-center m-4 p-4 rounded-lg bg-base-300  mx-auto"
           >
             <div>
-              <img className="w-20 h-20 rounded-full" src={photoUrl} alt="" />
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
             </div>
-            <div>
-              <h2 className="font-bold">{firstName + " " + lastName}</h2>
+            <div className="text-left mx-4 ">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
               {age && gender && <p>{age + ", " + gender}</p>}
               <p>{about}</p>
             </div>
             <div>
-              <button className="btn btn-primary mx-2">Reject</button>
-              <button className="btn btn-secondary mx-2">Accept</button>
+              <button
+                className="btn btn-primary mx-2"
+                onClick={() => reviewRequest("rejected", request._id)}
+              >
+                Reject
+              </button>
+              <button
+                className="btn btn-secondary mx-2"
+                onClick={() => reviewRequest("accepted", request._id)}
+              >
+                Accept
+              </button>
             </div>
           </div>
         );
@@ -52,5 +85,4 @@ const Requests = () => {
     </div>
   );
 };
-
 export default Requests;
